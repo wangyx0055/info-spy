34a076bc92ee4262c71af947dd8d65ba8bfe0600
boxizen <boxizen@gmail.com>
2015-11-19 15:22:20 +0800

    配置命令行参数

--- startup.js
+++ startup.js
@@ -1,8 +1,26 @@
 /* 
-* @Author: boxizen
-* @Date:   2015-11-19 00:42:37
-* @Last Modified by:   boxizen
-* @Last Modified time: 2015-11-19 00:42:37
-*/
+ * @Author: boxizen
+ * @Date:   2015-11-19 00:42:37
+ * @Last Modified by:   boxizen
+ * @Last Modified time: 2015-11-19 15:21:12
+ */
 
 'use strict';
+
+var program = require('commander'),
+
+    logger = console;
+
+function start() {
+    // 配置命令行参数
+    program.version('0.0.1')
+        .option('-u, --url <url>', 'url')
+        .parse(process.argv);
+
+    if (program.url) {
+        console.log('url:', program.url);
+    }
+    
+}
+
+start();
