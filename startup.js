09eaa5e7df2ed938a8b4e629960c798f0b1b7e91
boxizen <boxizen@gmail.com>
2015-12-02 17:42:06 +0800

    [UPD] 添加node集群工作

--- startup.js
+++ startup.js
@@ -2,15 +2,20 @@
  * @Author: boxizen
  * @Date:   2015-11-19 00:42:37
  * @Last Modified by:   boxizen
- * @Last Modified time: 2015-11-29 16:33:51
+ * @Last Modified time: 2015-12-02 17:41:17
  */
 
 'use strict';
 
 var program = require('commander'),
+    cluster = require('cluster'),
+    OS = require('os'),
 
     spy = require('./spy'),
     conf = require('./conf'),
+
+    cpus = OS.cpus().length,
+    maxClusterCount = cpus * 2,
 
     logger = console;
 
@@ -33,7 +38,18 @@ function start() {
     conf.init();
     spy.init();
 
-    // 运行spy
+    // 生产模式下启用多线程
+    if (!program.url && cluster.isMaster) {
+        for (var i = 0; i < cpus; i++) {
+            cluster.fork();
+        }
+        cluster.on('exit', function() {
+            console.log('A worker process died, restarting...');
+            cluster.fork();
+        });
+    }
+
+    // 调试模式
     if (program.url) {
         var task = {};
         task.url = program.url;
@@ -41,7 +57,10 @@ function start() {
             console.log(result.harvest);
         }
         spy.run(task);
-    } else {
+    }
+
+    // 生产模式
+    if (!cluster.isMaster) {
         spy.run();
     }
 }
